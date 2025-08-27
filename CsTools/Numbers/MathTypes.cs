dad52d096ce9c1528d962c2f93f61f86cc2bc51b
SpectralBlade <megajustdancer@gmail.com>
2025-08-27 12:35:16 +0200

    Initial commit

---
+++ CsTools/Numbers/MathTypes.cs
@@ -0,0 +1,74 @@
+﻿using System.Numerics;
+
+namespace FireBlade.CsTools.Numbers
+{
+    /// <summary>
+    /// Represents a range of numbers.
+    /// </summary>
+    /// <typeparam name="TNum">The number type.</typeparam>
+    public class Range<TNum> where TNum : INumber<TNum>
+    {
+        internal TNum _min;
+        internal TNum _max;
+
+        /// <summary>
+        /// Gets or sets the minimum value of the <see cref="Range{TNum}"/>.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public TNum Minimum
+        {
+            get => _min;
+            set
+            {
+                if (value > _max)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Minimum), "The minimum value is larger than the maximum value.");
+                }
+
+                _min = value;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the maximum value of the <see cref="Range{TNum}"/>.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public TNum Maximum
+        {
+            get => _max;
+            set
+            {
+                if (value < _min)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Minimum), "The minimum value is larger than the maximum value.");
+                }
+
+                _max = value;
+            }
+        }
+
+        // not using primary constructors because we need seperate documentation
+        /// <summary>
+        /// Creates a new instance of the <see cref="Range{TNum}"/> class.
+        /// </summary>
+        #pragma warning disable CS8618
+        public Range(TNum min, TNum max)
+        {
+            _min = min;
+            _max = max;
+        }
+#pragma warning restore
+
+        /// <summary>
+        /// Checks if the specified number is in the range.
+        /// </summary>
+        /// <param name="val">The value to test.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public bool IsInRange(TNum val) => val.IsInRange(this);
+
+        /// <summary>
+        /// Gets the difference between the <see cref="Maximum"/> value and the <see cref="Minimum"/> value.
+        /// </summary>
+        public TNum Length => _max - _min;
+    }
+}
---
+++ CsTools/Numbers/NumberExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System.Numerics;
+
+namespace FireBlade.CsTools.Numbers
+{
+    /// <summary>
+    /// Extends number types such as <see cref="int"/>, <see cref="float"/>, or <see cref="double"/>.
+    /// </summary>
+    public static class NumberExtensions
+    {
+        /// <summary>
+        /// Checks if the specified number is in a range.
+        /// </summary>
+        /// <typeparam name="TNum">The number type.</typeparam>
+        /// <param name="val">The value to test.</param>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public static bool IsInRange<TNum>(this TNum val, TNum min, TNum max) where TNum : INumber<TNum>
+        {
+            if (min > max)
+                throw new ArgumentOutOfRangeException(nameof(min), "Min is larger than max.");
+
+            return val >= min && val <= max;
+        }
+
+        /// <summary>
+        /// Checks if the specified number is in a range.
+        /// </summary>
+        /// <typeparam name="TNum">The number type.</typeparam>
+        /// <param name="val">The value to test.</param>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public static bool IsInRange<TNum>(this TNum val, int min, int max) where TNum : INumber<TNum>
+        {
+            return IsInRange(val, TNum.CreateTruncating(min), TNum.CreateTruncating(max));
+        }
+
+        /// <summary>
+        /// Checks if the specified number is in a range.
+        /// </summary>
+        /// <typeparam name="TNum">The number type.</typeparam>
+        /// <param name="val">The value to test.</param>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public static bool IsInRange<TNum>(this TNum val, float min, float max) where TNum : INumber<TNum>
+        {
+            return IsInRange(val, TNum.CreateTruncating(min), TNum.CreateTruncating(max));
+        }
+
+        /// <summary>
+        /// Checks if the specified number is in a range.
+        /// </summary>
+        /// <typeparam name="TNum">The number type.</typeparam>
+        /// <param name="val">The value to test.</param>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public static bool IsInRange<TNum>(this TNum val, double min, double max) where TNum : INumber<TNum>
+        {
+            return IsInRange(val, TNum.CreateTruncating(min), TNum.CreateTruncating(max));
+        }
+
+        /// <summary>
+        /// Checks if the specified number is in a range.
+        /// </summary>
+        /// <typeparam name="TNum">The number type.</typeparam>
+        /// <param name="val">The value to test.</param>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public static bool IsInRange<TNum>(this TNum val, decimal min, decimal max) where TNum : INumber<TNum>
+        {
+            return IsInRange(val, TNum.CreateTruncating(min), TNum.CreateTruncating(max));
+        }
+
+        /// <summary>
+        /// Checks if the specified number is in a range.
+        /// </summary>
+        /// <typeparam name="TNum">The number type.</typeparam>
+        /// <param name="val">The value to test.</param>
+        /// <param name="range">The range to check the value against.</param>
+        /// <returns><see langword="true"/> if the value is in range; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The minimum value is larger than the maximum value.</exception>
+        public static bool IsInRange<TNum>(this TNum val, Range<TNum> range) where TNum : INumber<TNum>
+        {
+            return IsInRange(val, range.Minimum, range.Maximum);
+        }
+    }
+}
